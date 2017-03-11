8ba827187cdee5bed96e52282eda44c8e51387f2
due2401 <hibik610@gmail.com>
2017-03-11 14:19:12 +0800

    將專案輸出改為類別庫

---
+++ CS280-Homework-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS280_Homework_2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
